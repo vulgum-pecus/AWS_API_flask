674dc7f552d82f8c99d0ffdb0b61d69551249de1
Chakib BELAFDIL <chakib.belafdil@gmail.com>
2023-07-08 10:55:42 +0200

    Modified app_flask.py in order to put TODOs in it. It allows student to easily check where to modify the script.

--- app_flask.py
+++ app_flask.py
@@ -1,23 +1,27 @@
+import pickle
+
 from flask import Flask, jsonify
 import numpy as np
 
 app = Flask(__name__)
 
 
+### TODO ### Modifier le chemin pour récupérer le modèle entrainé
+# model = pickle.load(open('path/to/model/model.pkl', 'rb'))
+### END TODO ###
+
 @app.route('/')
-def salut():
-    return 'Salut tout le Monde !'
-
-
-@app.route('/salut_perso/<string:first_name>')
-def salut_toi(first_name):
-    return f"Salut {first_name} !"
+def welcome():
+    return "Bienvenue dans l'API de prédiction des tags"
 
 
 @app.route('/predict_tags/<string:question>')
 def predict_tags(question):
-    all_tags = ['python', 'c++', 'java']
-    return jsonify(np.random.choice(all_tags))
+    ### TODO ### Mettre toutes les étapes de prétraitements permettant de réaliser la prédiction de tag de la question
+    all_tags = ['python', 'c++', 'java']  # A SUPPRIMER
+    # tag = model.predict(question)  # A ADAPTER
+    ### END TODO ###
+    return jsonify(np.random.choice(all_tags))  # A REMPLACER PAR : jsonify(tag)
 
 
 if __name__ == '__main__':
