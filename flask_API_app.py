2b47f580440b56a2be326291151dd60f97880382
Vulgum Pecus <vulgum_pecus@hotmail.fr>
2023-08-11 22:11:54 +0200

    Universal Sentence Encoder classifier applied to raw text corpus

--- flask_API_app.py
+++ flask_API_app.py
@@ -1,19 +1,52 @@
-### version tutoriel 
-
-from flask import Flask
+import tensorflow as tf
+import tensorflow_hub as hub
+import pickle
+import numpy as np
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def salut():
-    return 'Salut tout le Monde !'
+def bienvenue():
+    return "Bienvenue sur l'API de prédiction de tags pour une question du site StackOverFlow"
 
+@app.route('/predict_tags/<string:question>')
+def predict_tags(question):
 
-@app.route('/salut_perso/<string:first_name>')
-def salut_toi(first_name):
-    return f"Salut {first_name} !"
+    # 1. recupération des pickles pour le classifier et le multilabel binarizer
+    chemin_mlbinarizer = 'mlbinarizer.pkl'
+    with open(chemin_mlbinarizer, 'rb') as file:
+        mlbinarizer = pickle.load(file)
 
+    chemin_classifier = 'USEclassifier.pkl'
+    with open(chemin_classifier, 'rb') as file:
+        USEclassifier = pickle.load(file)
+
+    # 2. transform the question into USE vector
+    ## import trained USE model
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+    embedding_model = hub.load(module_url)
+
+    ## define the question as a list 
+    print(question)
+    question_list=[question]
+    
+    ## generate embeddings for our question
+    embedded_question = embedding_model(question_list)
+    print(embedded_question.shape)
+
+    # prédictions
+    # Predict on the tag using the best classifier
+    tag_pred = USEclassifier.predict(embedded_question)
+
+    # Transform predicted tags back to original format
+    prediction = mlbinarizer.inverse_transform(tag_pred)
+
+    data = {'suggested tags for your question' : prediction}
+
+    response=jsonify(data)
+    return response
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
