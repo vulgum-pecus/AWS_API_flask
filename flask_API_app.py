b7bd59f1b6b97ee54b29bb61d9e5913cb3c0e800
Vulgum Pecus <vulgum_pecus@hotmail.fr>
2023-08-14 16:13:36 +0200

    update = retrogradation sans spaCy tokenization à cause EC2

--- flask_API_app.py
+++ flask_API_app.py
@@ -1,19 +1,55 @@
-### version tutoriel 
+# version retrogradée pour compatibilité EC2
 
-from flask import Flask
+import tensorflow as tf
+import tensorflow_hub as hub
+import pickle
+import numpy as np
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def salut():
-    return 'Salut tout le Monde !'
+def bienvenue():
+    message = " *** Bienvenue sur l'API de prédiction de tags pour une question du site StackOverFlow ! *** \n \n Veuillez entrer, SVP, le endpoint predict_tags/ à la suite de l'URL de l'API et poser votre question. Le modèle vous suggérera des tags représentatifs du thème de votre question"
+    return "<pre>{}</pre>".format(message)
+
+@app.route('/predict_tags/<string:question>')
+def predict_tags(question):
+
+    # 1. recupération des pickles pour le classifier et le multilabel binarizer
+    chemin_mlbinarizer = 'mlbinarizer.pkl'
+    with open(chemin_mlbinarizer, 'rb') as file:
+        mlbinarizer = pickle.load(file)
+
+    chemin_classifier = 'USEclassifier.pkl'
+    with open(chemin_classifier, 'rb') as file:
+        USEclassifier = pickle.load(file)
+
+    # 2. transform the question into USE vector
+    ## import trained USE model
+    module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
+    embedding_model = hub.load(module_url)
+
+    ## define the question as a list 
+    print(question)
+    question_list=[question]
+    
+    ## generate embeddings for our question
+    embedded_question = embedding_model(question_list)
+    print(embedded_question.shape)
+
+    # prédictions
+    # Predict on the tag using the best classifier
+    tag_pred = USEclassifier.predict(embedded_question)
+
+    # Transform predicted tags back to original format
+    prediction = mlbinarizer.inverse_transform(tag_pred)
 
 
-@app.route('/salut_perso/<string:first_name>')
-def salut_toi(first_name):
-    return f"Salut {first_name} !"
-
+    response = {'Voici une suggestion de tags pour le contenu de votre question' : prediction}
+    
+    return jsonify(response)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
