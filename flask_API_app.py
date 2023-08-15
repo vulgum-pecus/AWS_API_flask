4f7ea16a4cc2efc4ae02cf61bb431ecd62083961
Vulgum Pecus <vulgum_pecus@hotmail.fr>
2023-08-15 09:52:05 +0200

    update = retrogradation sans spaCy tokenization à cause EC2

--- flask_API_app.py
+++ flask_API_app.py
@@ -11,7 +11,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def bienvenue():
-    message = " *** Bienvenue sur l'API de prédiction de tags pour une question du site StackOverFlow ! *** \n \n Veuillez entrer, SVP, le endpoint predict_tags/ à la suite de l'URL de l'API et poser votre question. Le modèle vous suggérera des tags représentatifs du thème de votre question"
+    message = ''' *** Bienvenue sur l'API de prédiction de tags pour une question du site StackOverFlow ! *** \n \n 
+
+    Veuillez entrer, SVP, le endpoint predict_tags/ à la suite de l'URL de l'API et poser votre question. \n \n 
+
+    Le modèle vous suggérera des tags représentatifs du thème de votre question'''
+    
     return "<pre>{}</pre>".format(message)
 
 @app.route('/predict_tags/<string:question>')
