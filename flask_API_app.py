fec7b58e75204f28f5179331d58e33e7831f11bb
Vulgum Pecus <vulgum_pecus@hotmail.fr>
2023-08-19 11:39:58 +0200

    scikit-learn version 1.2.1 annule et remplace version 1.0.2  et utilisation tokenization avec spacy

--- flask_API_app.py
+++ flask_API_app.py
@@ -1,9 +1,8 @@
-# version retrogradée classifier sans spacy tokenization pour alléger les requêtes sur le serveur EC2
-
 import tensorflow as tf
 import tensorflow_hub as hub
 import pickle
 import numpy as np
+import spacy
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -16,45 +15,43 @@ def bienvenue():
     Veuillez entrer, SVP, le endpoint /predict_tags/ à la suite de l'URL de l'API et poser votre question. \n \n 
 
     Le modèle vous suggérera des tags représentatifs du thème de votre question.'''
-    
     return "<pre>{}</pre>".format(message)
 
 @app.route('/predict_tags/<string:question>')
 def predict_tags(question):
-
-    # 1. recupération des pickles pour le classifier et le multilabel binarizer
-    chemin_mlbinarizer = 'mlbinarizer.pkl'
+    
+    # 1. tokenization, lemmatization  et conversion en liste
+    nlp = spacy.load("en_core_web_sm")
+    tokenized_question = nlp(question)
+    lemmatized_question = [token.lemma_ for token in tokenized_question if not (token.is_stop or token.is_space)]
+    lemmatized_question= [" ".join(lemmatized_question)]
+    
+   # 2. recupération des pickles pour le classifier et le multilabel binarizer
+    chemin_mlbinarizer = '230817_mlbinarizer.pkl'
     with open(chemin_mlbinarizer, 'rb') as file:
         mlbinarizer = pickle.load(file)
 
-    chemin_classifier = 'USEclassifier.pkl'
+    chemin_classifier = '230817_USEclassifier.pkl'
     with open(chemin_classifier, 'rb') as file:
-        USEclassifier = pickle.load(file)
+        classifier = pickle.load(file)
 
-    # 2. transform the question into USE vector
+    # 3. transformer la question en un vecteur UniversalSentenceEncoder
     ## import trained USE model
     module_url = "https://tfhub.dev/google/universal-sentence-encoder/4"
     embedding_model = hub.load(module_url)
-
-    ## define the question as a list 
-    print(question)
-    question_list=[question]
     
     ## generate embeddings for our question
-    embedded_question = embedding_model(question_list)
+    embedded_question = embedding_model(lemmatized_question)
     print(embedded_question.shape)
 
-    # prédictions
-    # Predict on the tag using the best classifier
-    tag_pred = USEclassifier.predict(embedded_question)
+    # 4. prédictions
+    tag_pred = classifier.predict(embedded_question)
 
-    # Transform predicted tags back to original format
+    ## Transform predicted tags back to original format
     prediction = mlbinarizer.inverse_transform(tag_pred)
 
-
-    response = {'Voici une suggestion de tags pour le contenu de votre question' : prediction}
-    
-    return jsonify(response)
+    response=jsonify({'Voici une suggestion de tags pour le contenu de votre question' : prediction})
+    return response
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
